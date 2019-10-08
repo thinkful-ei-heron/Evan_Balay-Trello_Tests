198b99cd5e123476e75cacda7a4e9aa489ed5c11
balayaydemir <balayaydemir@gmail.com>
2019-10-08 09:21:54 -0700

    final test done

--- src/list.test.js
+++ src/list.test.js
@@ -6,14 +6,14 @@ import List from './list';
 describe('<List />', () => {
     it ('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<List header="this is a card" card={['l', 'm']}/>, div);
+        ReactDOM.render(<List header="this is a card" card={[{ id: 'l' }, { id: 'm' }]}/>, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 });
 
 describe('<List />', () => {
     it('renders this UI as expected', () => {
-        const tree = renderer.create(<List header="this is a card" card={['l', 'm']}/>).toJSON();
+        const tree = renderer.create(<List header="this is a card" card={[{ id: 'l' }, { id: 'm' }]}/>).toJSON();
         expect(tree).toMatchSnapshot();
     });
 });
