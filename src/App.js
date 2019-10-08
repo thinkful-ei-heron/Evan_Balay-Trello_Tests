8cd0a0a618a343d0be1a9e147c0cd1c3d6338884
Evan Vogts <evanjvogts@gmail.com>
2019-10-08 10:15:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './app.css';
+import STORE from './store';
+import List from './list';
+
+function App(props) {
+  return (
+    <main className="App">
+    <header className="App-header">
+      <h1>Trelloyes!</h1>
+    </header>
+    <div className="App-list">
+      {STORE.lists.map(list => 
+        <List
+        key={list.id}
+        header={list.header}
+        card={list.cardIds.map(id =>
+          STORE.allCards[id]
+          )}/>
+      )}
+    </div>
+      
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './card.css'
+
+function card(props) {
+    return (
+        <div className="Card"> 
+        <button type="button">delete</button> 
+        <h3>{props.title}</h3> 
+        <p>{props.content}</p> 
+        </div>
+    )
+}
+
+export default card;
